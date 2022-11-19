c7c20374fce87637e75fcb650047c4b0ff5f0c5b
zacinthepark <zacinthepark@gmail.com>
2022-11-19 21:32:13 +0900

    get recommendation movies

--- vue-audisee/src/views/RecommendView.vue
+++ vue-audisee/src/views/RecommendView.vue
@@ -2,17 +2,29 @@
   <div>
     <h1>RecommendView</h1>
     <h1>비슷한 영화 추천</h1>
-    <RecommendItemVue />
+    <RecommendItem
+      v-for="movie in movies"
+      :key="movie.id"
+      :movie="movie"
+    />
   </div>
 </template>
 
 <script>
-import RecommendItemVue from "@/components/RecommendItem.vue";
+import RecommendItem from "@/components/RecommendItem.vue";
 
 export default {
   name: "RecommendView",
   components: {
-    RecommendItemVue,
+    RecommendItem,
+  },
+  computed: {
+    movies() {
+      return this.$store.state.movies
+    },
+  },
+  created() {
+      this.$store.dispatch('getMovies')
   },
 };
 </script>
