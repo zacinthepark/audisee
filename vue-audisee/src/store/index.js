abc12f9642a4cb5f7b38f2c94805b8e95747f611
zacinthepark <zacinthepark@gmail.com>
2022-11-20 18:16:43 +0900

    login and signup with token

--- vue-audisee/src/store/index.js
+++ vue-audisee/src/store/index.js
@@ -1,14 +1,19 @@
 import Vue from "vue"
 import Vuex from "vuex"
 import axios from 'axios'
+import createPersistedState from 'vuex-persistedstate'
 
 Vue.use(Vuex)
 
 const API_URL = "http://127.0.0.1:8000"
 
 export default new Vuex.Store({
+  plugins: [
+    createPersistedState()
+  ],
   state: {
     movies: [],
+    token: null,
   },
   getters: {
 
@@ -16,6 +21,9 @@ export default new Vuex.Store({
   mutations: {
     GET_MOVIES(state, movies) {
       state.movies = movies
+    },
+    SAVE_TOKEN(state, token) {
+      state.token = token
     }
   },
   actions: {
@@ -26,6 +34,39 @@ export default new Vuex.Store({
       })
         .then((response) => {
           context.commit('GET_MOVIES', response.data)
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+    },
+    signUp(context, payload) {
+      axios({
+        method: 'post',
+        url: `${API_URL}/accounts/signup/`,
+        data: {
+          username: payload.username,
+          password1: payload.password1,
+          password2: payload.password2,
+        }
+      })
+        .then((response) => {
+          context.commit('SAVE_TOKEN', response.data.key)
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+    },
+    logIn(context, payload) {
+      axios({
+        method: 'post',
+        url: `${API_URL}/accounts/login/`,
+        data: {
+          username: payload.username,
+          password: payload.password,
+        }
+      })
+        .then((response) => {
+          context.commit('SAVE_TOKEN', response.data.key)
         })
         .catch((error) => {
           console.log(error)
--- vue-audisee/src/views/SignupView.vue
+++ vue-audisee/src/views/SignupView.vue
@@ -9,6 +9,18 @@
     <div class="d-flex align-items-center justify-content-center">
       <div class="text-start">
         <h1>Signup</h1>
+        <form @submit.prevent="signUp">
+          <label for="username">아이디: </label>
+          <input type="text" id="username" v-model="username"><br>
+
+          <label for="password1">비밀번호: </label>
+          <input type="text" id="password1" v-model="password1"><br>
+
+          <label for="password2">비밀번호 확인: </label>
+          <input type="text" id="password2" v-model="password2"><br>
+
+          <input type="submit" value="회원가입">
+        </form>
       </div>
     </div>
   </div>
@@ -17,6 +29,28 @@
 <script>
 export default {
   name: "SignupView",
+  data() {
+    return {
+      username: null,
+      password1: null,
+      password2: null,
+    }
+  },
+  methods: {
+    signUp() {
+      const username = this.username
+      const password1 = this.password1
+      const password2 = this.password2
+
+      const payload = {
+        username: username,
+        password1: password1,
+        password2: password2,
+      }
+
+      this.$store.dispatch('signUp', payload)
+    }
+  }
 };
 </script>
 
