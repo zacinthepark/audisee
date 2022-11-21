8c818bc81c948f82a3d70ee4e13e39470f8693a9
Ming0Kim <minofficial13@gmail.com>
2022-11-21 17:57:02 +0900

    feat login

--- vue-audisee/src/views/LoginView.vue
+++ vue-audisee/src/views/LoginView.vue
@@ -6,17 +6,44 @@
     >
       <button class="btn btn-outline-light" type="button">Signup</button>
     </router-link>
-    <div class="d-flex align-items-center justify-content-center">
-      <div class="text-start">
-        <h1>Login</h1>
+    <div class="card bg-transparent border-secondary">
+      <h3 class="card-header">
+        <b-navbar-brand href="Home" class="text-white tw-bold"
+          >AudiSee</b-navbar-brand
+        >
+      </h3>
+      <div class="card-body">
+        <h4 class="card-subtitle my-1">Audisee에 로그인하세요</h4>
         <form @submit.prevent="logIn">
-          <label for="username">아이디: </label>
-          <input type="text" id="username" v-model="username"><br>
-
-          <label for="password">비밀번호: </label>
-          <input type="text" id="password" v-model="password"><br>
-
-          <input type="submit" value="로그인">
+          <div class="m-3 row card-text fs-5">
+            <label for="username" class="col-sm-2 col-form-label"
+              >아이디
+            </label>
+            <div>
+              <input
+                type="text"
+                class="form-control"
+                id="username"
+                v-model="username"
+              />
+            </div>
+          </div>
+          <div class="m-3 row card-text fs-5">
+            <label for="password" class="col-sm-2 col-form-label"
+              >비밀번호
+            </label>
+            <div>
+              <input
+                type="password"
+                class="form-control"
+                id="password"
+                v-model="password"
+              /><br />
+            </div>
+          </div>
+          <button type="submit" value="LogIn" class="btn btn-outline-light">
+            Login
+          </button>
         </form>
       </div>
     </div>
@@ -30,19 +57,19 @@ export default {
     return {
       username: null,
       password: null,
-    }
+    };
   },
   methods: {
     logIn() {
-      const username = this.username
-      const password = this.password
+      const username = this.username;
+      const password = this.password;
       const payload = {
         username: username,
         password: password,
-      }
-      this.$store.dispatch('logIn', payload)
-    }
-  }
+      };
+      this.$store.dispatch("logIn", payload);
+    },
+  },
 };
 </script>
 
--- vue-audisee/src/views/SignupView.vue
+++ vue-audisee/src/views/SignupView.vue
@@ -6,20 +6,57 @@
     >
       <button class="btn btn-outline-light" type="button">Login</button>
     </router-link>
-    <div class="d-flex align-items-center justify-content-center">
-      <div class="text-start">
-        <h1>Signup</h1>
+    <div class="card bg-transparent border-secondary">
+      <h3 class="card-header">
+        <b-navbar-brand href="Home" class="text-white tw-bold"
+          >AudiSee</b-navbar-brand
+        >
+      </h3>
+      <div class="card-body">
+        <h4 class="card-subtitle my-1">Audisee에 가입하세요</h4>
         <form @submit.prevent="signUp">
-          <label for="username">아이디: </label>
-          <input type="text" id="username" v-model="username"><br>
-
-          <label for="password1">비밀번호: </label>
-          <input type="text" id="password1" v-model="password1"><br>
-
-          <label for="password2">비밀번호 확인: </label>
-          <input type="text" id="password2" v-model="password2"><br>
-
-          <input type="submit" value="회원가입">
+          <div class="m-3 row card-text fs-5">
+            <label for="username" class="col-sm-2 col-form-label"
+              >아이디
+            </label>
+            <div>
+              <input
+                type="text"
+                class="form-control"
+                id="username"
+                v-model="username"
+              />
+            </div>
+          </div>
+          <div class="m-3 row card-text fs-5">
+            <label for="password1" class="col-sm-2 col-form-label"
+              >비밀번호
+            </label>
+            <div>
+              <input
+                type="password"
+                class="form-control"
+                id="password1"
+                v-model="password1"
+              />
+            </div>
+          </div>
+          <div class="m-3 row card-text fs-5">
+            <h6 for="password2" class="col-sm-2 col-form-label">
+              비밀번호 확인
+            </h6>
+            <div>
+              <input
+                type="password"
+                class="form-control"
+                id="password2"
+                v-model="password2"
+              />
+            </div>
+          </div>
+          <button type="submit" value="SignIn" class="btn btn-outline-light">
+            Signup
+          </button>
         </form>
       </div>
     </div>
@@ -34,22 +71,22 @@ export default {
       username: null,
       password1: null,
       password2: null,
-    }
+    };
   },
   methods: {
     signUp() {
-      const username = this.username
-      const password1 = this.password1
-      const password2 = this.password2
+      const username = this.username;
+      const password1 = this.password1;
+      const password2 = this.password2;
 
       const payload = {
         username: username,
         password1: password1,
         password2: password2,
-      }
-      this.$store.dispatch('signUp', payload)
-    }
-  }
+      };
+      this.$store.dispatch("signUp", payload);
+    },
+  },
 };
 </script>
 
